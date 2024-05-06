16da599ae7f17c8778f449568676dbead9469b68
xiaoming0016 <45249700+xiaoming0016@users.noreply.github.com>
2024-05-06 21:41:36 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,51 @@
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+// GLFW
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    
+    // 创建一个窗口对象
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+    if (window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+
+    // 主循环
+    while (!glfwWindowShouldClose(window))
+    {
+        // 检查并调用事件
+        glfwPollEvents();
+
+        // 交换缓冲
+        glfwSwapBuffers(window);
+    }
+
+    // 退出前清理已分配的GLFW资源
+    glfwTerminate();
+    return 0;
+}
